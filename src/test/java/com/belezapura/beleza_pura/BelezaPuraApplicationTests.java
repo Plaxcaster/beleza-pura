9593f7ecd657588e7cb5a3bf8802bce235eb42c8
Pedro Plá <pedro.bpla@gmail.com>
2025-07-17 22:23:16 -0300

    Initial commit using git initialzr

---
+++ src/test/java/com/belezapura/beleza_pura/BelezaPuraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.belezapura.beleza_pura;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BelezaPuraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
